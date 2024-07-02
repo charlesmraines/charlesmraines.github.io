cb2b99d4453c16b67f56e9a6cf83c19777d5cf50
Charles M. Raines <charlesraines30@outlook.com>
2024-07-02 16:23:06 -0500

    init commit 2

--- react-app/src/components/Navbar/Navbar.jsx
+++ react-app/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Navbar.module.css"
+import {getImageUrl} from "../../utils.js"
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return <nav className={styles.navbar}>
         <a className={styles.title} href="/">Charles M. Raines</a>
         <div className={styles.menu}>
-            <ul className={styles.menuItems}>
+            <img 
+                className={styles.menuBtn} 
+                src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")} 
+                alt="menu-button"
+                onClick={() => setMenuOpen(!menuOpen)}
+            />
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                onClick = {() => setMenuOpen(false)}
+            >
                 <li><a href="#about">About</a></li>
                 <li><a href="#Experience">Experience</a></li>
                 <li><a href="#Projects">Projects</a></li>
